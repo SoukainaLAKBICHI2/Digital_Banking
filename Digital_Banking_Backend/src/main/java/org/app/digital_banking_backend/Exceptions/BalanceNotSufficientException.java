1a0087bb043f437de7833fe845e38f68aca3823c
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-05-05 14:06:50 +0100

    Implementation des classes des exceptions

---
+++ Digital_Banking_Backend/src/main/java/org/app/digital_banking_backend/Exceptions/BalanceNotSufficientException.java
@@ -0,0 +1,7 @@
+package org.app.digital_banking_backend.Exceptions;
+
+public class BalanceNotSufficientException extends Exception {
+    public BalanceNotSufficientException(String message) {
+        super(message);
+    }
+}
---
+++ Digital_Banking_Backend/src/main/java/org/app/digital_banking_backend/Exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package org.app.digital_banking_backend.Exceptions;
+
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
