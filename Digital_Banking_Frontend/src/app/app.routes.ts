189ab347d84187c651dfe9bd70ade99ea3935904
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-05-07 13:11:19 +0100

    Securite Frontend page login

--- Digital_Banking_Frontend/src/app/app.routes.ts
+++ Digital_Banking_Frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {AccountsComponent} from './accounts/accounts.component';
 import {EditCustomerComponent} from './edit-customer/edit-customer.component';
 import {NewCustomerComponent} from './new-customer/new-customer.component';
 import {CustomerAccountsComponent} from './customer-accounts/customer-accounts.component';
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'customers', pathMatch: 'full' },
   { path: 'customers', component: CustomersComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'accounts', component: AccountsComponent },
   { path: 'add-customer', component: NewCustomerComponent},
   { path: 'customer-accounts', component: CustomerAccountsComponent},
