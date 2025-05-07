2e024b1546c7c6c3d6553625752c720ed7205f91
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-05-07 13:20:20 +0100

    Ajouter Admin-template

--- Digital_Banking_Frontend/src/app/app.routes.ts
+++ Digital_Banking_Frontend/src/app/app.routes.ts
@@ -5,14 +5,19 @@ import {EditCustomerComponent} from './edit-customer/edit-customer.component';
 import {NewCustomerComponent} from './new-customer/new-customer.component';
 import {CustomerAccountsComponent} from './customer-accounts/customer-accounts.component';
 import {LoginComponent} from './login/login.component';
+import {AdminTemplateComponent} from './admin-template/admin-template.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'customers', pathMatch: 'full' },
-  { path: 'customers', component: CustomersComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'accounts', component: AccountsComponent },
-  { path: 'add-customer', component: NewCustomerComponent},
-  { path: 'customer-accounts', component: CustomerAccountsComponent},
-  {
-    path: 'edit-customer/:id', component: EditCustomerComponent}
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo: "/login", pathMatch:"full"},
+  {path:"admin", component:AdminTemplateComponent, children:[
+      {path:"customers", component: CustomersComponent},
+      {path:"accounts", component: AccountsComponent},
+      { path :"new-customer", component : NewCustomerComponent},
+      { path :"customer-accounts/:id", component : CustomerAccountsComponent},
+      {
+        path: 'edit-customer/:id', component: EditCustomerComponent}
+    ]},
+
+
 ];
