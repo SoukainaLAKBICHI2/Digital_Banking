928aa7dd4d84906b28603f0aafe4b491ff6af4a3
SoukainaLAKBICHI <soukaina.lakbichi-etu@etu.univh2c.ma>
2025-05-07 13:36:28 +0100

    Implementer les intercepteurs

--- Digital_Banking_Frontend/src/app/app.config.ts
+++ Digital_Banking_Frontend/src/app/app.config.ts
@@ -2,8 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient } from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {appHttpInterceptor} from './interceptors/app-http.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([appHttpInterceptor])
+    ),
+  ]
 };
